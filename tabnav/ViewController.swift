55f07fbc1b880961570b9aafe8be97ca15f4ac70
kan.notice <kan@notice.co.jp>
2014-11-04 16:44:12 +0900

    Initial Commit

---
+++ tabnav/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tabnav
+//
+//  Created by kan on 2014/11/04.
+//  Copyright (c) 2014年 notice,inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
